f66609360962c0b04ea7624eaf35b4da70ccd138
klezi10 <74952593+klezi10@users.noreply.github.com>
2022-01-22 09:53:33 +0700

    moved nav code to the top

--- script.js
+++ script.js
@@ -1,27 +1,3 @@
-const footer = document.createElement('footer');
-
-footer.innerHTML = `
-<p>Blogspot</p>
-      <p>
-        This project was coded by
-        <a
-          href="https://clever-turing-3f9e94.netlify.app/"
-          target="_blank"
-          title="Portfolio link"
-          >Klesta Luli</a
-        >
-        |
-        <a
-          href="https://github.com/klezi10/blogspot"
-          target="_blank"
-          title="Github code"
-          >Open-source</a
-        >
-      </p>
-`;
-
-document.querySelector('.footer').append(footer);
-
 // =====================================NAVBAR=============
 
 const navbar = document.createElement('ul');
@@ -46,3 +22,29 @@ function highlightCurrentURL() {
 window.onload = function () {
   highlightCurrentURL();
 };
+
+// ====== FOOTER =====
+
+const footer = document.createElement('footer');
+
+footer.innerHTML = `
+<p>Blogspot</p>
+      <p>
+        This project was coded by
+        <a
+          href="https://clever-turing-3f9e94.netlify.app/"
+          target="_blank"
+          title="Portfolio link"
+          >Klesta Luli</a
+        >
+        |
+        <a
+          href="https://github.com/klezi10/blogspot"
+          target="_blank"
+          title="Github code"
+          >Open-source</a
+        >
+      </p>
+`;
+
+document.querySelector('.footer').append(footer);
